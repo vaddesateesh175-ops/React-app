915f51149c3992c55a703dd8a52c5c2dc5f3722e
sateesh <vaddesateesh175@gmail.com>
2026-03-12 19:53:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./component/Navbar";
+import Counter from "./features/Counter";
+
+function App(){
+    return(<div>
+      <h1>hi react app</h1>
+      <Navbar></Navbar>
+      <Counter></Counter>
+    </div>)
+}
+export default App;
---
+++ src/features/Counter.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Button from "@mui/material/Button";
+function ButtonUsage(props) {
+  return <Button variant="contained">{props.children}</Button>;
+}
+function Counter() {
+  const [count, setCount] = React.useState(0);
+  function inc(){
+    setCount(count+1)
+  }
+  return (
+    <div className="mybox">
+      <h1>Counter:{count}</h1>
+      <button onClick={()=>{inc()}}>increment</button>
+    </div>
+  );
+}
+
+export default Counter;
