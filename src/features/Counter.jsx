37ce9e881d47c859a0476a9273aa4cf699a3ba20
sateesh <vaddesateesh175@gmail.com>
2026-03-12 19:59:31 +0530

    Counter

--- src/features/Counter.jsx
+++ src/features/Counter.jsx
@@ -8,10 +8,14 @@ function Counter() {
   function inc(){
     setCount(count+1)
   }
+  function dec(){
+    setCount(count-1)
+  }
   return (
     <div className="mybox">
       <h1>Counter:{count}</h1>
       <button onClick={()=>{inc()}}>increment</button>
+      <button onClick={()=>{dec()}}>decrement</button>
     </div>
   );
 }
