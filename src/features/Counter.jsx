b8c5216363a4dd439716f3db8a4b3423f6b00f2b
sateesh <vaddesateesh175@gmail.com>
2026-03-13 05:54:25 +0530

    setter-callback

--- src/features/Counter.jsx
+++ src/features/Counter.jsx
@@ -6,12 +6,20 @@ function ButtonUsage(props) {
 function Counter() {
   const [count, setCount] = React.useState(0);
   function inc(){
-    setCount(count+1)
+    setCount((c)=>{
+      return(c+1)
+    })
+  }
+  function dec(){
+    setCount((c)=>{
+      return(c-1)
+    })
   }
   return (
     <div className="mybox">
       <h1>Counter:{count}</h1>
       <button onClick={()=>{inc()}}>increment</button>
+      <button onClick={()=>{dec()}}>decrement</button>
     </div>
   );
 }
